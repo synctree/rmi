a8d81b3669299c7830938a0d21e7c62b8c59415b
Blake Petetan <blake@blake.me>
2012-05-22 11:22:12 -0700

    adding apache solr to homebrew formulas

---
+++ homebrews/solr/3.6.0/solr.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+class Solr < Formula
+  url 'http://www.apache.org/dyn/closer.cgi?path=lucene/solr/3.6.0/apache-solr-3.6.0.tgz'
+  homepage 'http://lucene.apache.org/solr/'
+  md5 'f59ed35cf2b8c8130774425797d95f5d'
+
+  def script; <<-EOS.undent
+    #!/bin/sh
+    if [ -z "$1" ]; then
+      echo "Usage: $ solr path/to/config/dir"
+    else
+      cd #{libexec}/example && java -Dsolr.solr.home=$1 -jar start.jar
+    fi
+    EOS
+  end
+
+  def install
+    libexec.install Dir['*']
+    (bin+'solr').write script
+  end
+
+  def caveats; <<-EOS.undent
+    To start solr:
+        solr path/to/solr/config/dir
+
+    See the solr homepage for more setup information:
+        brew home solr
+    EOS
+  end
+end
